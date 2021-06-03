e5fdc86aa122fe093fefb650c2affcf230f93e87
sarmory <sarmory@hotmail.com>
2021-06-03 16:58:44 +0100

    Update tblTask.sql

--- tblTask.sql
+++ tblTask.sql
@@ -16,4 +16,14 @@ CREATE TABLE [dbo].[tblTask](
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 
-
+INSERT INTO [dbo].[tblTask]
+           ([TaskName],[TaskSQL],[TaskTarget])
+VALUES ('Transform1', 'SELECT FLOOR(RAND() * 1000) AS SomeNumber','tblTaskSomeStuff1'),
+('Transform2', 'SELECT SomeNumber FROM  dbo.tblTaskSomeStuff1','tblTaskSomeStuff2'),
+('Transform3','SELECT SomeNumber FROM  dbo.tblTaskSomeStuff4','tblTaskSomeStuff3'),
+('Transform4','SELECT SomeNumber FROM  dbo.tblTaskSomeStuff2','tblTaskSomeStuff4'),
+('Transform5','SELECT * FROM  dbo.tblTaskSomeStuff4 UNION SELECT * FROM dbo.tblTaskSomeStuff7 UNION SELECT * FROM dbo.tblTaskSomeStuff8','tblTaskSomeStuff5'),
+('Transform6','SELECT * FROM  dbo.tblTaskSomeStuff3 UNION SELECT * FROM dbo.tblTaskSomeStuff5 UNION SELECT * FROM dbo.tblTaskSomeStuff7','tblTaskSomeStuff6'),
+('Transform7','SELECT * FROM  dbo.tblTaskSomeStuff1 UNION SELECT * FROM  dbo.tblTaskSomeStuff8','tblTaskSomeStuff7'),
+('Transform8','SELECT FLOOR(RAND() * 1000) AS SomeNumber','tblTaskSomeStuff8')
+GO
