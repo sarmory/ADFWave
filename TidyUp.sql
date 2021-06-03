9669a67aa9188abe12baa86ee6b7ad4ae00abafa
sarmory <sarmory@hotmail.com>
2021-06-03 16:37:16 +0100

    Create TidyUp.sql

---
+++ TidyUp.sql
@@ -0,0 +1,33 @@
+/****** Object:  Table [dbo].[tblTaskSomeStuff4]    Script Date: 03/06/2021 10:49:06 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblTaskSomeStuff1]') AND type in (N'U'))
+DROP TABLE [dbo].[tblTaskSomeStuff1]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblTaskSomeStuff2]') AND type in (N'U'))
+DROP TABLE [dbo].[tblTaskSomeStuff2]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblTaskSomeStuff3]') AND type in (N'U'))
+DROP TABLE [dbo].[tblTaskSomeStuff3]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblTaskSomeStuff4]') AND type in (N'U'))
+DROP TABLE [dbo].[tblTaskSomeStuff4]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblTaskSomeStuff5]') AND type in (N'U'))
+DROP TABLE [dbo].[tblTaskSomeStuff5]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblTaskSomeStuff6]') AND type in (N'U'))
+DROP TABLE [dbo].[tblTaskSomeStuff6]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblTaskSomeStuff7]') AND type in (N'U'))
+DROP TABLE [dbo].[tblTaskSomeStuff7]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tblTaskSomeStuff8]') AND type in (N'U'))
+DROP TABLE [dbo].[tblTaskSomeStuff8]
+GO
+
